07d5abc4c0387443d5ad5650ed020d4e3c331c08
amdpt323 <95157589+amdpt323@users.noreply.github.com>
2022-12-22 21:26:58 +0530

    added firebase auth and email verification

--- flutter_crud_app/lib/main.dart
+++ flutter_crud_app/lib/main.dart
@@ -1,258 +1,68 @@
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_crud_app/firebase_options.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_crud_app/pages/AuthPage.dart';
+
+import 'package:flutter_crud_app/Widgets/Utils.dart';
+import 'package:flutter_crud_app/pages/verifyEmail.dart';
+
+
+
+
 
 
 void main() async{
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );runApp(MaterialApp(
+  );runApp(MyApp());}
 
-  debugShowCheckedModeBanner: false,
-  theme: ThemeData(
-    brightness: Brightness.light,
-    primaryColor: Colors.blue,
-    accentColor: Colors.cyan,
-  ),
-  home: MyApp(),
-));}
+final navigatorKey = GlobalKey <NavigatorState> ();
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) => MaterialApp(
+
+    navigatorKey: navigatorKey,
+    scaffoldMessengerKey: messengerKey,
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      brightness: Brightness.light,
+      primaryColor: Colors.blue,
+
+    ),
+    home: MainPage(),
+  );
 }
 
-class _MyAppState extends State<MyApp> {
-  late String studentName;
-  late String studentID;
-  late String studentBranch;
-  late double studentCpi;
 
-  getStudentName(name){
-    this.studentName=name;
-  }
-  getStudentID(studentID){
-    this.studentID=studentID;
-  }
-
-  getStudentBranch(branch){
-    this.studentBranch=branch;
-  }
-
-  getStudentCPI(cpi){
-    this.studentCpi = double.parse(cpi);
-  }
-
-  createData(){
-    
-
-    DocumentReference documentReference = FirebaseFirestore.instance.
-    collection("studentData").doc(studentName);
-
-    Map<String , dynamic>students={
-      "studentName":studentName,
-      "studentID":studentID,
-      "branch":studentBranch,
-      "cpi":studentCpi,
-    };
-
-    documentReference.set(students).whenComplete(() =>{
-      print("$studentName created")
-    } );
-  }
-
-  readData(){
-    DocumentReference documentReference = FirebaseFirestore.instance
-        .collection("studentData").doc(studentName);
-    
-    documentReference.get().then((dataSnapshot){
-      print(dataSnapshot["studentName"]);
-      print(dataSnapshot["studentID"]);
-      print(dataSnapshot["branch"]);
-      print(dataSnapshot["cpi"]);
-    });
-  }
-
-  updateData(){
-    DocumentReference documentReference = FirebaseFirestore.instance.
-    collection("studentData").doc(studentName);
-
-    Map<String , dynamic>students={
-      "studentName":studentName,
-      "studentID":studentID,
-      "branch":studentBranch,
-      "cpi":studentCpi,
-    };
-
-    documentReference.set(students).whenComplete(() =>{
-      print("$studentName updated")
-    } );
-  }
-
-  deleteData(){
-    DocumentReference documentReference = FirebaseFirestore.instance.
-    collection("studentData").doc(studentName);
-
-    documentReference.delete().whenComplete(() => {
-      print("$studentName deleted")
-    });
-  }
-
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My flutter CRUD app'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children:<Widget>[
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: "Name",
-                  fillColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color:Colors.blue,
-                      width: 2.0,
-                    )
-                  )
-                ),
-                onChanged: (String name){
-                  getStudentName(name);
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    labelText: "Sutdent ID",
-                    fillColor: Colors.white,
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color:Colors.blue,
-                          width: 2.0,
-                        )
-                    )
-                ),
-                onChanged: (String studentID){
-                  getStudentID(studentID);
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    labelText: "Branch",
-                    fillColor: Colors.white,
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color:Colors.blue,
-                          width: 2.0,
-                        )
-                    )
-                ),
-                onChanged: (String branch){
-                  getStudentBranch(branch);
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    labelText: "CPI",
-                    fillColor: Colors.white,
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color:Colors.blue,
-                          width: 2.0,
-                        )
-                    )
-                ),
-                onChanged: (String cpi){
-                  getStudentCPI(cpi);
-                },
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget> [
-                ElevatedButton(
-                  style:ButtonStyle(
-                    backgroundColor:MaterialStateProperty.all<Color>(Colors.green) ,
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16)
-                    )),
+  Widget build(BuildContext context) => Scaffold(
+    body: StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context,snapshot){
+        if(snapshot.connectionState==ConnectionState.waiting){
+          return Center(child: CircularProgressIndicator(),);
+        }else if(snapshot.hasError){
+          return Center(child: Text('Something went wrong'),);
+        }else if(snapshot.hasData){
+          return VerifyEmailPage();
+        }else{
+          return AuthPage();
+        }
 
-                  ) ,
-                  child: Text("create",style: TextStyle(color: Colors.white),),
-
-                  onPressed: (){
-                    createData();
-                  },
-                ),
-                ElevatedButton(
-                  style:ButtonStyle(
-                    backgroundColor:MaterialStateProperty.all<Color>(Colors.blue) ,
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16)
-                    )),
-
-                  ) ,
-                  child: Text("read",style: TextStyle(color: Colors.white),),
-
-                  onPressed: (){
-                    readData();
-
-                  },
-                ),
-                ElevatedButton(
-                  style:ButtonStyle(
-                    backgroundColor:MaterialStateProperty.all<Color>(Colors.orange) ,
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16)
-                    )),
-
-                  ) ,
-                  child: Text("update",style: TextStyle(color: Colors.white),),
-
-                  onPressed: (){
-                    updateData();
-                  },
-                ),
-                ElevatedButton(
-                  style:ButtonStyle(
-                    backgroundColor:MaterialStateProperty.all<Color>(Colors.red) ,
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16)
-                    )),
-
-                  ) ,
-                  child: Text("delete",style: TextStyle(color: Colors.white),),
-
-                  onPressed: (){
-                    deleteData();
-                  },
-                )
-              ],
-            ),
-            // -+
-          ],
-        ),
-      ),
-    );
-  }
+      },
+    )
+  );
 }
+
+
